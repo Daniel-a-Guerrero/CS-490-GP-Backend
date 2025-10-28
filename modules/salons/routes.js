8a2cdf7afc45a66aaf0eab277c7b7816d7f79cd0
Hager Shahin <hagershahin@Hagers-MBP.attlocal.net>
2025-10-28 08:26:48 -0400

    fix: path change

--- modules/salons/routes.js
+++ modules/salons/routes.js
@@ -1,11 +1,10 @@
-const express=require('express');
-const { verifyFirebaseToken } = require("../middleware/firebaseAuth");
-const router=express.Router();
-const db=require('../database');
-const salonController=require('../controllers/salonControllers');
+const express = require('express');
+const { verifyFirebaseToken } = require("../../middleware/firebaseAuth");
+const router = express.Router();
+const salonController = require('./controller');
 
-router.get('/?freeDays', verifyFirebaseToken, salonController.getAllSalons);
+router.get('/', verifyFirebaseToken, salonController.getAllSalons);
 router.get('/:salonId/staff', verifyFirebaseToken, salonController.getStaffBySalonId);
 router.get('/staff/schedule', verifyFirebaseToken, salonController.getDailySchedule);
 router.get('/user/visit-history', verifyFirebaseToken, salonController.getUserVisitHistory);
-module.exports=router;
+module.exports = router;
